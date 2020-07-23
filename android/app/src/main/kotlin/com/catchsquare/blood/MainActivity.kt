8fb128fdcc78ad0de4965c4d9ed7308c24e8cf7f
Ravikant Paudel <developer.paudel@gmail.com>
2020-07-23 13:19:28 +0545

    Initial code

---
+++ android/app/src/main/kotlin/com/catchsquare/blood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.catchsquare.blood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,21 @@
+import 'package:blood/domain/core/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
