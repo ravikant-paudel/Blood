2c9ba74c2a9e47472bb7f8e96cb2441eb32cc1d0
Ravikant Paudel <developer.paudel@gmail.com>
2020-07-24 00:01:33 +0545

    Implemented Firebase Auth in project

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,20 @@
+import 'package:blood/domain/auth/auth_failure.dart';
+import 'package:blood/domain/auth/value_objects.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+
+  Future<Either<AuthFailure, Unit>> signInWithFacebook();
+}
