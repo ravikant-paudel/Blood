a697d7d0ca819cea5629a186d000f32ddf6d9da1
Ravikant Paudel <developer.paudel@gmail.com>
2020-07-27 15:35:56 +0545

    "Minor-Fix"

---
+++ lib/features/main_page.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 The Bloog Authors. All rights reserved.
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+//      body: ExtendedNavigator(
+//        initialRoute: Routes.dashboardPage,
+//        router: Router(),
+//        name: 'mainRouter',
+//      ),
+//      bottomNavigationBar: KhaltiBottomNavBar(),
+    );
+  }
+}
