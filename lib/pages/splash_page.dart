54d2252d035d89d15fe28ea9c80a098bde7812a0
Ravikant <ravikant.paudel@gmail.com>
2022-01-11 14:15:18 +0545

    Fixed splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,14 +1,27 @@
 // Copyright (c) 2020 The Blood Authors. All rights reserved.
 
 import 'package:blood/component/gaps.dart';
+import 'package:blood/helper/router/go_router.dart';
 import 'package:blood/utils/resources/dimens.dart';
 import 'package:blood/utils/shortcuts.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      goRouter.go('/');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
